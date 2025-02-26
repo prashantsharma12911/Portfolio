c9f417d459fd958da5bc08d3281a11af0c206f47
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2025-02-26 17:16:50 +0530

    fixed sm device profile card and added actions for icons

--- src/components/PortfolioHeading.tsx
+++ src/components/PortfolioHeading.tsx
@@ -1,18 +1,41 @@
-import { Text, Image, HStack, Box } from "@chakra-ui/react"
-import github from '../assets/github.svg'
-import linkedin from '../assets/linkedin.svg'
-import mail from '../assets/mail.svg'
+import { Text, Image, HStack, Box, Icon, Tooltip, useClipboard, Alert, AlertDescription, AlertTitle, useToast, Link } from "@chakra-ui/react"
+import { SiMaildotru as mail } from "react-icons/si"
+import { FaGithub as github } from "react-icons/fa";
+import { TiSocialLinkedinCircular as linkedin } from "react-icons/ti";
 
 const PortfolioHeading = () => {
+    const gmail = "prashant.kumar.12911@gmail.com";
+    const toast = useToast()
+    const { onCopy } = useClipboard(gmail);
+
+
+    const mailClickAction = () => {
+        onCopy();
+        toast({
+            title: 'Mail copied.',
+            status: 'success',
+            duration: 1000,
+            isClosable: true,
+        });
+    };
     return (
         <>
             <Text fontFamily='Raleway' color='black' fontWeight='bold' fontSize='2xl' marginY='8'>Hi, I am</Text>
             <Text fontFamily='Raleway' color='black' fontWeight='extrabold' fontSize='5xl' marginY={-3}>Prashant Sharma</Text>
             <Text fontFamily='Raleway' color='#909090' fontWeight='semibold' fontSize='2xl' >Full Stack Software Engineer</Text>
-            <HStack marginY={10} spacing={5}>
-                <Image src={mail} boxSize={8} boxShadow={4} borderRadius="md" border="2px solid black" padding={1} />
-                <Image src={github} boxSize={8} boxShadow={4} borderRadius="md" border="2px solid black" padding={1} />
-                <Image src={linkedin} boxSize={8} boxShadow={4} borderRadius="md" border="2px solid black" padding={1} />
+            <HStack marginY={10} spacing={10}>
+                <Icon as={mail} boxSize={10} boxShadow={4} cursor='pointer' onClick={() => { mailClickAction() }} />
+                <Link href='https://github.com/prashantsharma12911' isExternal>
+
+                    <Icon as={github} boxSize={10} boxShadow={4} cursor='pointer' />
+
+                </Link>
+
+                <Link href='https://www.linkedin.com/in/prashant-kumar-5497a116a/' isExternal>
+                    <Icon as={linkedin} boxSize={14} boxShadow={4} cursor='pointer' />
+
+                </Link>
+
             </HStack>
         </>
     )
--- src/components/ProfileHeaderSm.tsx
+++ src/components/ProfileHeaderSm.tsx
@@ -1,22 +1,48 @@
-import { Box, Image, Text, HStack, Grid, SimpleGrid, GridItem, Flex } from '@chakra-ui/react'
+import { Box, Image, Text, HStack, Grid, SimpleGrid, GridItem, Flex, Icon, Link, useToast, useClipboard } from '@chakra-ui/react'
 import React from 'react'
 import sharma from '../assets/sharma.jpeg'
 import github from '../assets/github.svg'
 import linkedin from '../assets/linkedin.svg'
-import mail from '../assets/mail.svg'
+import { SiMaildotru } from "react-icons/si"
+import { FaGithub } from "react-icons/fa";
+import { TiSocialLinkedinCircular } from "react-icons/ti";
+
+
 const ProfileHeaderSm = () => {
+
+    const gmail = "prashant.kumar.12911@gmail.com";
+    const toast = useToast()
+    const { onCopy } = useClipboard(gmail);
+
+
+    const mailClickAction = () => {
+        onCopy();
+        toast({
+            title: 'Mail copied.',
+            status: 'success',
+            duration: 1000,
+            isClosable: true,
+        });
+    };
+
+
     return (
         <Box display='flex' >
             <Box w='70vw' h='30vh' display='flex' flexDirection='column' justifyContent='center'>
-                 <Text fontFamily='Raleway' color='white' fontWeight='bold'  >Hi, I am</Text>
-             <Text as='span' marginX={2} fontFamily='Raleway' color='white' fontWeight='extrabold' fontSize='3xl' >Prashant Sharma</Text>
-            <Text fontFamily='Raleway' color='white' fontWeight='semibold'  >Full Stack Software Engineer</Text>
+                <Text fontFamily='Raleway' color='white' fontWeight='bold'  >Hi, I am</Text>
+                <Text as='span' marginX={2} fontFamily='Raleway' color='white' fontWeight='extrabold' fontSize='3xl' >Prashant Sharma</Text>
+                <Text fontFamily='Raleway' color='white' fontWeight='semibold'  >Full Stack Software Engineer</Text>
 
             </Box>
-            <Box w='30vw' h='30vh'  display="flex" flexDirection="column" gap={5}  >
-                     <Image src={mail} bg='white'  borderRadius="full" overflow="hidden" boxSize={10} boxShadow={4}  border="1px solid black"  />
-                 <Image src={github} boxSize={10} boxShadow={4} borderRadius="full" overflow="hidden" border="1px solid black"  />
-                <Image src={linkedin} boxSize={10} boxShadow={4} borderRadius="full" overflow="hidden" border="1px solid black"  />
+            <Box w='30vw' h='30vh' display="flex" flexDirection="column" gap={5}  >
+                <Icon as={SiMaildotru} boxSize={10} cursor='pointer' onClick={() => mailClickAction()} />
+                <Link href='https://github.com/prashantsharma12911' isExternal>
+                    <Icon as={FaGithub} boxSize={10} />
+                </Link>
+                <Link href='https://www.linkedin.com/in/prashant-kumar-5497a116a/' isExternal>
+                    <Icon as={TiSocialLinkedinCircular} boxSize={10} />
+
+                </Link>
 
             </Box>
         </Box>
