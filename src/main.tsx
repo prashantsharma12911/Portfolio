58744bfea099ee1729f0ef11f0b9a3829ebf6b3b
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2025-03-23 22:26:18 +0530

    routing added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
+import { RouterProvider } from 'react-router-dom';
+import router from './routing/routing';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      {/* <App /> */}
+      <RouterProvider router={router}/>
     </ChakraProvider>
   </React.StrictMode>,
 )
